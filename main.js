caeeebbd9bac5963a2d254f5e265156ee1d29d7e
VRosa-jpg <vixkirosa@gmail.com>
2024-11-12 20:49:45 -0500

    update

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
+const BASE_URL = 'https://anime-facts-rest-api.herokuapp.com/api/v1'
 
-console.log("JS IS WORKING")
+// Show facts on page
+const facts = document.querySelector(".facts")
+// Whenever they click here a new pic and cat shows up
+const button = document.querySelector("button")
 
-// Select the elements we are going to use
-const button = document.querySelector("#pickYourDog");
-const breedInput = document.querySelector("#breedInput");
-const imageDiv = document.querySelector("#dogPicture"); // Make sure to use the correct ID with "#"
-const middle_class = document.querySelector('.middle')
 
-// Add an event listener to the button
-button.addEventListener('click', async () => {
-  let breed = breedInput.value;
+async function change() {
 
-  try {
-    // Make the GET request using axios
-    let response = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random`);
+  console.log("Hello")
 
-    console.log(response); // Log the full response for debugging
-    let dogPic = response.data.message; // Access the image URL from the response
-    console.log(dogPic); // Log the image URL
-
-    // Now, it must be displayed
-    imageDiv.innerHTML = `<img src="${dogPic}" alt="A random dog of breed ${breed}">`;
-  } catch (error) {
-    console.error("Error fetching dog image:", error); // Log any errors
-    imageDiv.innerHTML = "<p>Sorry, we couldn't find a dog image.</p>";
-  }
-});
-
-middle_class.addEventListener('reload', async () => {
-
-})
+  fetch(`${BASE_URL}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  
+}
